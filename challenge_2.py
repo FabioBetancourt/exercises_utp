bf5592e4dbe57a10b7c6bc436064b0d8cf30497e
FabioBetancourt <fabiobetancourt7@gmail.com>
2022-05-13 19:11:14 -0500

    added changes

--- challenge_2.py
+++ challenge_2.py
@@ -1,10 +1,5 @@
 
-
-
-
-
 def cliente (informacion:dict)->dict:
-    pass
     if informacion["edad"] > 18 and informacion["primer_ingreso"]==True:
         atraccion = 'X-Treme'
         total_boleta=20000*0.95
@@ -14,11 +9,11 @@ def cliente (informacion:dict)->dict:
         total_boleta=20000
         apto=True
     elif informacion["edad"] >= 15 and informacion["edad"] <= 18 and informacion["primer_ingreso"]==True:   
-        atraccion = "Carroschocones"
+        atraccion = "Carros chocones"
         total_boleta=5000*0.93
         apto=True
     elif informacion["edad"] >= 15 and informacion["edad"] <= 18 and informacion["primer_ingreso"]==False:   
-        atraccion = "Carroschocones"
+        atraccion = "Carros chocones"
         total_boleta=5000
         apto=True
     elif informacion["edad"] >= 7 and informacion["edad"] < 15 and informacion["primer_ingreso"]==True:
@@ -34,11 +29,9 @@ def cliente (informacion:dict)->dict:
         atraccion="N/A"
         total_boleta="N/A"
     
-    return {'nombre': informacion["nombre"], 'edad': informacion["edad"],'atraccion':atraccion,'apto':apto,'primer_ingreso': informacion["primer_ingreso"], 'total_boleta': total_boleta}
+    return {'nombre':informacion["nombre"],'edad': informacion["edad"],'atraccion':atraccion,'apto':apto,'primer_ingreso':informacion["primer_ingreso"],'total_boleta':total_boleta}
  
     
-        
-
 informacion = {
     'id_cliente': 1,
     'nombre': 'Johana Fernandez',
@@ -49,6 +42,7 @@ print(cliente(informacion))
 
 
 '''
+{'nombre': 'Johana Fernandez', 'edad': 20, 'atraccion': 'X-Treme', 'apto': True, 'primer_ingreso': True, 'total_boleta': 19000.0}
 {'nombre': 'Johana Fernandez', 'edad': 20, 'atraccion': 'X-Treme', 'apto': True, 'primer_ingreso': True, 'total_boleta': 19000.0}
 {'nombre': 'Johana Fernandez', 'edad': 20, 'atraccion': 'X-Treme', 'apto': True, 'primer_ingreso': True, 'total_boleta': 19000.0}
 
