75708e4c799f329aa35a8eee770c887f20cd5d6a
FabioBetancourt <fabiobetancourt7@gmail.com>
2022-05-22 14:10:22 -0500

    added a cycle for the bot

--- challenge_3.py
+++ challenge_3.py
@@ -1,35 +1,26 @@
-def AutoPartes(ventas: list):
+def AutoPartes(agenda: list):
     ventas={} #Inicializar diccionario
-    for IdProducto,dProducto,pnProducto,cvProducto,sProducto,nComprador,cComprador,fVenta in ventasg:         #Ciclo para agregar un nuevo evento
-        if ventas.get(IdProducto) == None:             #Fuerza la entrada
-            ventas[IdProducto] = []                    #Creacion de un nuevo evento
-        ventas[IdProducto].append((dProducto,pnProducto,cvProducto,sProducto,nComprador,cComprador,fVenta))  #Registro de una nueva lista de tuplas en el dict agenda
+    for IdProducto,dProducto,pnProducto,cvProducto,sProducto,nComprador,cComprador,fVenta in agenda:   #Ciclo para agregar un nuevo evento
+        if ventas.get(IdProducto ) == None:             #Fuerza la entrada
+            ventas[IdProducto ] = []                    #Creacion de un nuevo evento
+        ventas[IdProducto ].append((dProducto,pnProducto,cvProducto,sProducto,nComprador,cComprador,fVenta))  #Registro de una nueva lista de tuplas en el dict ventas
     return ventas
 
+
 def consultaRegistro(ventas, idproducto): 
-    AutoPartes()
-    if idproducto == 2010:
-        print ('Producto consultado : {idProducto} Descripción {dProducto} #Parte {pnProducto} Cantidad vendida {cvProducto} Stock {sProducto} Comprador {nComprador} Documento {cComprador} Fecha Venta {fVenta}')
-    elif idproducto == 9852:
-       print ('Producto consultado : {idProducto} Descripción {dProducto} #Parte {pnProducto} Cantidad vendida {cvProducto} Stock {sProducto} Comprador {nComprador} Documento {cComprador} Fecha Venta {fVenta}')
+    partlog=''
+    if idproducto in ventas.keys():
+        for values in ventas[idproducto]:
+            partlog='Producto consultado : '+str(idproducto)+'  Descripción  '+str(values[0])+'  #Parte  '+str(values[1])+'  Cantidad vendida  '+str(values[2])+'  Stock  '+str(values[3])+'  Comprador '+str(values[4])+'  Documento  '+str(values[5])+'  Fecha Venta  '+str(values[6])
+            partlog=partlog[::]
+            print(partlog)
     else:
         print ("No hay registro de venta de ese producto")
 
 
-#Producto consultado : 2010 Descripción bujía #Parte MS9512 Cantidad vendida 4 Stock 15 Comprador Carlos Rondon Documento 1256 Fecha Venta 12/06/2020
-#Producto consultado : 2010 Descripción bujía #Parte ER6523 Cantidad vendida 9 Stock 36 Comprador Pedro Montes Documento 1243 Fecha Venta 12/06/2020
-
-#Producto consultado : 2010 Descripción bujía #Parte MS9512 Cantidad vendida 4 Stock 15 Comprador Carlos Rondon Documento 1256 Fecha Venta 12/06/2020
-#Producto consultado : 2010 Descripción bujía #Parte ER6523 Cantidad vendida 9 Stock 36 Comprador Pedro Montes Documento 1243 Fecha Venta 12/06/2020
-
-
-ventasg=([
+consultaRegistro(AutoPartes([
  (2001,'rosca', 'PT29872',2,45,'Luis Molero',3456,'12/06/2020'),
  (2010,'bujía', 'MS9512',4,15,'Carlos Rondon',1256,'12/06/2020'),
  (2010,'bujía', 'ER6523',9,36,'Pedro Montes',1243,'12/06/2020'),
  (3578,'tijera', 'QW8523',1,128,'Pedro Faria',1456,'12/06/2020'),
- (9251,'piñón', 'EN5698',2,8,'Juan Peña',565,'12/06/2020')])
-
-
-
-consultaRegistro(AutoPartes(ventasg), 9852)
+ (9251,'piñón', 'EN5698',2,8,'Juan Peña',565,'12/06/2020')]),2010) 
